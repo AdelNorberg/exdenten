9963ca65952301bca6e364fa2090dc5e38da1dfb
Adel <darzemanov101@mail.ru>
2018-11-13 18:54:28 +0300

    init app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
+   <el-container>
+    <el-header>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+    <el-footer>
+      2018 @AdelNorberg
+    </el-footer>
+  </el-container>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="sass" scoped>
+.el-header 
+  background-color: #fff
+  color: #333
+  text-align: center
+  line-height: 60px
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08)
+  z-index: 100
+
+
+.el-footer 
+  background-color: #f0f2f5
+  color: #333
+  text-align: center
+  line-height: 60px
+
+
+.el-main 
+  background-color: #f0f2f5
+  color: #333
+  text-align: center
+  line-height: 160px
+
+
+body > .el-container 
+  min-height: 100vh
+
 </style>
